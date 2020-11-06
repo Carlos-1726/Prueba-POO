d60409853c8a2b7c127fdc95b15bc3c66ef26c6b
dighero1726@gmail.com <dighero1726@gmail.com>
2020-11-05 22:24:41 -0600

    Vista ya completa

--- Proyecto Final/src/com/gcb/cdc/vista/Vista.java
+++ Proyecto Final/src/com/gcb/cdc/vista/Vista.java
@@ -1,13 +1,13 @@
 package com.gcb.cdc.vista;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.GridLayout;
-
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
 
 public class Vista extends JFrame {
 	
@@ -19,13 +19,14 @@ public class Vista extends JFrame {
 	public JRadioButton hipertension;
 	public JRadioButton obesidad;
 	public JRadioButton edad;
-	public Button estados;
+	public JButton estados;
 	public JPanel panel1,panel2, panelbotons;
 	
 	public Vista(){
 		
 		super("Proyecto Final");
 		setSize(900,600);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocation(300,100);
 		setResizable(true);
@@ -42,21 +43,37 @@ public class Vista extends JFrame {
 		panelbotons.setLayout(new GridLayout(4,2));
 		
 		hombre = new JRadioButton("Hombre");
+		hombre.setHorizontalAlignment(SwingConstants.CENTER);
 		panelbotons.add(hombre);
+		
 		mujer = new JRadioButton("Mujer");
+		mujer.setHorizontalAlignment(SwingConstants.CENTER);
 		panelbotons.add(mujer);
+		
 		epoc = new JRadioButton("EPOC");
+		epoc.setHorizontalAlignment(SwingConstants.CENTER);
 		panelbotons.add(epoc);
+		
 		asma = new JRadioButton("Asma");
+		asma.setHorizontalAlignment(SwingConstants.CENTER);
 		panelbotons.add(asma);
-		hipertension = new JRadioButton("Hipertension");
+		
+		hipertension = new JRadioButton("Hipertensión");
+		hipertension.setHorizontalAlignment(SwingConstants.CENTER);
 		panelbotons.add(hipertension);
+		
 		obesidad = new JRadioButton("Obesidad");
+		obesidad.setHorizontalAlignment(SwingConstants.CENTER);
 		panelbotons.add(obesidad);
+		
 		edad = new JRadioButton("Edad");
+		edad.setHorizontalAlignment(SwingConstants.CENTER);
 		panelbotons.add(edad);
 		
 		panel1.add(panelbotons,BorderLayout.CENTER);
+		
+		estados = new JButton("Mostrar Grafica");
+		panel1.add(estados,BorderLayout.SOUTH);
 		
 		this.add(panel1);
 		
